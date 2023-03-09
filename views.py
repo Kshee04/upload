e3927823946edbd02c968b182a35677136fe693e
Kshee04 <123535084+Kshee04@users.noreply.github.com>
2023-03-09 23:52:44 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 
 # Create views
 def store(request):
-    products= Product.objects.all()
+    products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store.html', context)
 
@@ -15,18 +16,30 @@ def index(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'checkout.html', context)
 
 
 def cart(request):
-
     if request.user.is_authenticated:
-        customer=request.user.customer
-        order, created= Order.objects.get_or_create(customer= customer, complete= False)
-        items= order.orderitem_set.all()
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
     else:
-        items= []
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context={'items':items}
+    context = {'items': items, 'order': order}
     return render(request, 'cart.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
